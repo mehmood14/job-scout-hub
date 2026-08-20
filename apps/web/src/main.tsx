4cfb6ed4a1a1cda0b733a50ab83f5686afa8dfb7
Mehmood ul haq <mehmoodulhaq14@gmail.com>
2026-08-20 15:26:20 +0200

    feat: add Vercel web analytics

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,6 +4,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "../src/app/App.tsx";
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Analytics />
     </QueryClientProvider>
   </StrictMode>,
 );
